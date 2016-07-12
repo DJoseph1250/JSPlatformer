f9c357c91cd6c86b610f8953717af34127e0db67
djoseph1250 <djoseph1250@gmail.com>
2016-07-12 10:19:20 -0400

    defined game objects

--- code/game.js
+++ code/game.js
@@ -1,3 +1,9 @@
+var actorChars = {
+	Player: "@",
+	Coin: "o",
+	Lava: ["=", "|", "v"],
+};
+
 function Level(plan) {
 	this.width = plan[0].length;
 	this.height = plan.length;
@@ -30,3 +36,50 @@ function Level(plan) {
 Level.prototype.isFinished = function(first_argument) {
 	return this.status != null && this.finishDelay < 0;
 };
+
+function Vector(x, y) {
+	this.x = x;
+	this.y = y;
+}
+
+Vector.prototype.plus = function(other) {
+	return new Vector(this.x + other.x, this.y + other.y);
+};
+
+Vector.prototype.times = function(factor) {
+	return new Vector(this.x * factor, this.y * factor);
+};
+
+function Player(pos) {
+	this.pos = pos.plus(new Vector(0, -0.5));
+	this.size = new Vector(0.8, 1.5);
+	this.speed = new Vector(0, 0);
+}
+
+Player.prototype.type = "player";
+
+function Lava(pos, ch) {
+	this.pos = pos;
+	this.size = new Vector(1, 1);
+	if (ch == "=") {
+		this.speed = new Vector(2, 0);
+	} else if (ch == "|") {
+		this.speed = new Vector(0, 2);
+	} else if (ch == "v") {
+		this.speed = new Vector(0, 3);
+		this.repeatPos = pos;
+	}
+}
+
+Lava.prototype.type = "lava";
+
+function Coin(pos) {
+	this.pos = pos;
+	this.size = new Vector(0.6, 0.6);
+	this.wobble = Math.random() * Math.PI * 2;
+}
+
+Coin.prototype.type = "coin";
+
+var simpleLevel = new Level(simpleLevelPlan);
+console.log(simpleLevel.width, "by", simpleLevel.height);
